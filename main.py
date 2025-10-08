46b1871abe072d85c26e099864aa6f290e554a6b
Michelle <mpuschkarow@web.de>
2025-10-08 12:46:49 +0200

    Right answers stores in a list which is used to show the score

--- main.py
+++ main.py
@@ -13,22 +13,30 @@ def get_mouse_click_coor(x,y):
     print(x,y)
 turtle.onscreenclick(get_mouse_click_coor)
 
-game_over = False
+#Game setup
+guessed_states = []
 
-while not game_over:
+while len(guessed_states) < 50:
     #User Input
-    answer_state = screen.textinput(title="Guess the State",  prompt="What´s another state´s name?")
-
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 Guess the State",
+                                    prompt="What´s another state´s name?").title()
+    print(answer_state)
     #Check user answer, get coordinates
     data = pandas.read_csv("./data/50_states.csv")
     state_list = data["state"].to_list()
+
     for state in state_list:
-
         if answer_state == state:
-            found_row = data[data.state == answer_state]
-            print(f"State found: {answer_state}.")
-            #create turtle to write name of the state
-        else:
-            print("This is no U.S. state.")
 
-    turtle.mainloop() #alternative way to keep screen open
+            #Create turtle to write name of the state
+            t = turtle.Turtle()
+            t.hideturtle()
+            t.up()
+            state_row = data[data.state == answer_state]
+            t.goto(state_row.x.item(), state_row.y.item())
+            t.write(f"{state}")
+            if answer_state not in guessed_states:
+                guessed_states.append(state)
+
+turtle.mainloop() #alternative way to keep screen open
+screen.exitonclick()
